cdc3122552ac64bd72cc480d6634901b39049875
ivokkkkk <pedroivo9141@outlook.com>
2025-03-19 19:27:53 -0300

    Add files via upload

---
+++ html-front/script.js
@@ -0,0 +1,26 @@
+   /* console.log(7+3);
+    console.log("Hello");
+    console.log("World");   
+    console.log(27*3);
+
+    var PrimeiroTipo;
+    let SegundoTipo;
+    const TerceiroTipo = 1;
+
+    let nome = "Pedro";
+    let idade = "20";
+
+    console.log(nome);
+    console.log(idade);
+
+    nome = idade;
+
+    console.log(nome); */
+
+    function hello(){
+        document.getElementById('texto').textContent = "HELLO WORLD";
+        document.getElementById('texto').style.color = "blue"
+
+    }
+
+    
